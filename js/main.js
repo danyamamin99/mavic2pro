4f912757aec594fe2dda93c706ee33128793c410
danyamamin99 <danya.mamin.99@bk.ru>
2020-10-14 15:30:36 +0300

    day 3

--- js/main.js
+++ js/main.js
@@ -5,5 +5,16 @@ $(function(){
   nextArrow: '<button class="slider__btn slider__btn-right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
   infinite: false,
  });
+// 
+ $('.questions__item-title').on('click', function () {
+  $('.questions__item').removeClass('questions__item--active');
+  $(this).parent().addClass('questions__item--active');
+ });
+// 
+$('#fullpage').fullpage({
+		autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
+	});
 
 });
